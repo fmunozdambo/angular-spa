4deedebba18b5735743470de060e41503dac8d45
fmunozdambo <fmunozdambo@gmail.com>
2020-10-25 10:05:14 +0100

    Create components and routes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  selector: 'app-home',
+  templateUrl: './home.component.html'
 })
-export class NavbarComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
   constructor() { }
 
