d6bf577e1f9df23c725c75eefa3fbd8f11296328
fmunozdambo <fmunozdambo@gmail.com>
2020-10-25 11:38:18 +0100

    Add single hero page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 import { AboutComponent } from './components/about/about.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'heroes', component: HeroesComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'heroe/:id', component: HeroeComponent },
     { path: '**',  pathMatch: 'full', redirectTo: 'home'}
 ];
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styles: [
-  ]
+  selector: './heroes.component.html',
+  templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService,
+              private _router: Router) {
+    
+   }
 
   ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
+  verHeroe(idx: number){
+    this._router.navigate(['/heroe',idx]);
+  }
 }
